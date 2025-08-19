ef3ab25f635ebaa322260a996e576f47bee9e237
sinclair <haydn.developer@gmail.com>
2025-08-19 13:43:12 +0900

    Revision 0.8.6

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 <h1>Tasksmith</h1>
 
-<p>Automation Tooling for Deno</p>
+<p>Command Line Automation for Deno</p>
 
 <img src="tasksmith.png" />
 
@@ -15,21 +15,20 @@
 
 ## Overview
 
-Tasksmith is a lightweight task automation tool for Deno
+Tasksmith is a command line automation tool for Deno. It offers a CLI router that routes tasks to TypeScript functions. It also includes a suite of built in functions to assist with project management workflows.
 
 License: MIT
 
 ## Usage
 
-Tasksmith provides a command-line router that routes tasks based on the first command-line argument passed to Deno. Additional arguments are passed into the task function. Task files can be run from anywhere, but would usually reside in a `tasks.ts` file within a project root. The following is a
-simple task file.
+Tasksmith is a command-line router that routes tasks based name.
 
 ```typescript
 import { Task } from 'https://raw.githubusercontent.com/sinclairzx81/tasksmith/0.8.0/src/index.ts'
 
-Task.run('test', async (...args: string[]) => {/* ... */})
+Task.run('test', () => console.log('test'))
 
-Task.run('build', async (...args: string[]) => {/* ... */})
+Task.run('build', () => console.log('build'))
 ```
 
 Tasks are usually configured and run from `deno.json`
@@ -44,47 +43,3 @@ Tasks are usually configured and run from `deno.json`
 ```
 
 Check [here](https://github.com/sinclairzx81/tasksmith/tags) for the latest tagged version.
-
-## Shell
-
-The `Task` namespace provides a `shell(...)` function to run and orchestrate command-line programs.
-
-```typescript
-// Run a command line program
-Task.run('start', async () => Task.shell('deno run -A example/index.ts'))
-
-// ... or run them in parallel
-Task.run('serve', () =>
-  Promise.all([
-    Task.shell('deno run -A example/client.ts'),
-    Task.shell('deno run -A example/server.ts'),
-  ]))
-```
-
-## Build
-
-The `Task` namespace also provides a `build(...)` function which creates a npm publishable package from a Deno source tree. Internally this function uses TypeScript for compilation and the `attw` command line CLI for module resolution verification. The output is a dual `cjs` and `esm` package that
-uses the [package json redirects](https://github.com/andrewbranch/example-subpath-exports-ts-compat/tree/main/examples/node_modules/package-json-redirects) resolution scheme.
-
-```typescript
-Task.run('build', () =>
-  Task.build('src-build', {
-    compiler: '5.8.2', // tsc compiler version
-    outdir: 'dist',
-    additional: ['license', 'readme.md'],
-    packageJson: {
-      name: '@sinclair/project',
-      description: 'A software library',
-      version: '1.0.0',
-      author: 'user',
-      license: 'MIT',
-      keywords: ['tooling', 'automation'],
-      repository: {
-        type: 'git',
-        url: 'https://github.com/sinclairzx81/project',
-      },
-    },
-  }))
-```
-
-Refer to the `src-tree` directory with in this repository for an example of how modules should be structured.
--- tasks.ts
+++ tasks.ts
@@ -3,21 +3,61 @@ import { Task } from './src/index.ts'
 // ------------------------------------------------------------------
 // Clean
 // ------------------------------------------------------------------
-Task.run('clean', async () => {
-  await Task.folder('target').delete()
-})
+Task.run('clean', () => Task.folder('target').delete())
 // ------------------------------------------------------------------
 // Format
 // ------------------------------------------------------------------
-Task.run('format', async () => {
-  await Task.shell('deno fmt src')
-})
+Task.run('format', () => Task.shell('deno fmt src'))
 // ------------------------------------------------------------------
 // Start
 // ------------------------------------------------------------------
-Task.run('start', async () => {
-  await Task.shell('deno run -A --watch example/index.ts')
-})
+Task.run('start', () => Task.shell('deno run -A --watch example/index.ts'))
+// ------------------------------------------------------------------
+// Serve
+// ------------------------------------------------------------------
+Task.run('serve', () => Task.serve('ref/serve'))
+// ------------------------------------------------------------------
+// Build: Dual
+// ------------------------------------------------------------------
+Task.run('build:dual', () =>
+  Task.build.dual('ref/build', {
+    compiler: 'latest',
+    outdir: 'target/build',
+    additional: ['license', 'readme.md'],
+    packageJson: {
+      name: '@sinclair/project',
+      description: 'A software library',
+      version: '1.0.0',
+      author: 'sinclairzx81',
+      license: 'MIT',
+      keywords: ['tooling', 'automation'],
+      repository: {
+        type: 'git',
+        url: 'https://github.com/sinclairzx81/project',
+      },
+    },
+  }))
+// ------------------------------------------------------------------
+// Build: Esm
+// ------------------------------------------------------------------
+Task.run('build:esm', () =>
+  Task.build.esm('ref/build', {
+    compiler: 'latest',
+    outdir: 'target/build',
+    additional: ['license', 'readme.md'],
+    packageJson: {
+      name: '@sinclair/project',
+      description: 'A software library',
+      version: '1.0.0',
+      author: 'sinclairzx81',
+      license: 'MIT',
+      keywords: ['tooling', 'automation'],
+      repository: {
+        type: 'git',
+        url: 'https://github.com/sinclairzx81/project',
+      },
+    },
+  }))
 // ------------------------------------------------------------------
 // Metrics
 // ------------------------------------------------------------------
@@ -29,64 +69,12 @@ Task.run('metrics', async () => {
     'src-build/baz/baz.ts',
   ])
 })
-
-// ------------------------------------------------------------------
-// Build: Dual
-// ------------------------------------------------------------------
-Task.run('build:dual', () =>
-  Task.build.dual('src-build', {
-    compiler: 'latest',
-    outdir: 'target/build',
-    additional: ['license', 'readme.md'],
-    packageJson: {
-      name: '@sinclair/project',
-      description: 'A software library',
-      version: '1.0.0',
-      author: 'user',
-      license: 'MIT',
-      keywords: ['tooling', 'automation'],
-      repository: {
-        type: 'git',
-        url: 'https://github.com/sinclairzx81/project',
-      },
-    },
-  }))
-
-// ------------------------------------------------------------------
-// Build: Esm
-// ------------------------------------------------------------------
-Task.run('build:esm', () =>
-  Task.build.esm('src-build', {
-    compiler: 'latest',
-    outdir: 'target/build',
-    additional: ['license', 'readme.md'],
-    packageJson: {
-      name: '@sinclair/project',
-      description: 'A software library',
-      version: '1.0.0',
-      author: 'user',
-      license: 'MIT',
-      keywords: ['tooling', 'automation'],
-      repository: {
-        type: 'git',
-        url: 'https://github.com/sinclairzx81/project',
-      },
-    },
-  }))
-
 // ------------------------------------------------------------------
 // Test
 // ------------------------------------------------------------------
 Task.run('test', () => Task.test.run(['test/subset0', 'test/subset1']))
-
 // ------------------------------------------------------------------
 // Report
 // ------------------------------------------------------------------
 Task.run('report', () => Task.test.report(['test/subset0', 'test/subset1']))
 
-// ------------------------------------------------------------------
-// Serve
-// ------------------------------------------------------------------
-Task.run('serve', async () => {
-  await Task.serve('src-serve/index.html', { port: 5000 })
-})
